a871ae3087b0013800c466bbdb4b0275886de055
Abdoulaye DIALLO <vonewman7@gmail.com>
2023-07-19 20:13:12 +0000

    add customers.csv

---
+++ tp_dbt/models/staging/tracks3_4_Minutes.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized="table"
+         )
+}}
+
+WITH data_source AS(
+SELECT TrackId, Name, AlbumId, MediaTypeId, GenreId, Milliseconds, Bytes
+FROM {{ source("staging","tracks") }}
+),
+final AS (
+    SELECT * FROM data_source
+    WHERE Milliseconds >= 180000 AND Milliseconds <= 240000 AND Bytes > 6600000
+
+)
+
+SELECT * FROM final
